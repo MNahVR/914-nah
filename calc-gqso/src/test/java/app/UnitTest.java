70dd55f818e1e457e7e4f32b70281a6c1a79c4a7
MNahVR <mnvr1@aluno.ifal.edu.br>
2021-08-20 04:44:29 +0000

    1.5º soma gqso

---
+++ calc-gqso/src/test/java/app/UnitTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+  @Test
+  public void welcome() {
+    Controlador controlador = new Controlador();
+    assertEquals("Welcome to Jooby!", controlador.sayHi());
+  }
+}
